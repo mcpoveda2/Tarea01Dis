f4bde3cd0b7ea63ef3dc244bb021c01affb06b8f
Jonthz <jnzambra@fiec.edu.espol.ec>
2024-06-09 16:28:40 -0500

    added subject

---
+++ src/observer/Subject.java
@@ -0,0 +1,19 @@
+package observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subject {
+    private List<Observer> observers = new ArrayList<>();
+
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void notifyObservers(String message) {
+        for (Observer observer : observers) {
+            observer.update(message);
+        }
+    }
+}
+
